c77cad01230d212c330d204792619ebd69bda9ed
dungpt3 <phamtiendung0926@gmail.com>
2024-10-29 20:41:09 +0700

    fix lint

--- app/components/local-switcher.tsx
+++ app/components/local-switcher.tsx
@@ -9,23 +9,30 @@ function LocalSwitcher() {
     const t = useTranslations('Label');
     const router = useRouter();
     const localActive = useLocale();
+    
     const onSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
         const nextLocale = e.target.value;
         startTransition(() => {
-            router.replace(`/${nextLocale}`)
-        })
+            router.replace(`/${nextLocale}`);
+        });
     }
-  return (
-    <div>
-        <label>
-            <p>{t('change-language')}</p>
-            <select defaultValue={localActive} onChange={onSelectChange}>
-                <option value="en">English</option>
-                <option value="vi">Việt Nam</option>
-            </select>
-        </label>
-    </div>
-  )
+
+    return (
+        <div>
+            <label>
+                <p>{t('change-language')}</p>
+                <select
+                    defaultValue={localActive}
+                    onChange={onSelectChange}
+                    disabled={isPending}
+                >
+                    <option value="en">English</option>
+                    <option value="vi">Việt Nam</option>
+                </select>
+            </label>
+            {isPending && <p>Loading...</p>}
+        </div>
+    )
 }
 
 export default LocalSwitcher
