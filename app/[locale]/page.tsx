5ec902d79577ce63400ef1850c6a8d334fba92ec
dungpt3 <phamtiendung0926@gmail.com>
2024-10-29 20:30:04 +0700

    update source base

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,7 +2,19 @@ import {useTranslations} from 'next-intl';
 import {Link} from '@/i18n/routing';
 import { Button } from '@mui/material';
 import MyForm from '../components/MyForm';
+import { Metadata } from 'next';
  
+export const metadata: Metadata = {
+  title: "Trang chủ",
+  description: "Trang chủ của công ty",
+  openGraph: {
+    title: "Trang chủ",
+    description: "Trang chủ của công ty",
+    url: "https://yourwebsite.com",
+  },
+  keywords: ["Trang chủ", "công ty", "tên công ty"],
+};
+
 export default function HomePage() {
   const t = useTranslations('HomePage');
   return (
